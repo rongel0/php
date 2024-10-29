d4f2596429c40af9bc1ae7e4312379142b635786
rongel0 <takahiro.122@docomo.ne.jp>
2024-10-29 16:50:08 +0900

    PHP-5

--- sample.php
+++ sample.php
@@ -1,19 +1,49 @@
 <?php 
-$array = [1,3,5,7,9,11,13];
 
-for($i = 0; $i < count($array); ++$i ){
+class Company{
+    protected $company;
+    protected $address;
+    protected $tel;
+
+    protected function __construct($company, $address, $tel) {
+        $this->company = $company;
+        $this->address = $address;
+        $this->tel = $tel;
+    }
     
-    if($array[$i] < 13 ){
-        echo $array[$i].PHP_EOL;
-        continue;
+    protected function displayCompany(){
+        echo "会社名：".$this->company.PHP_EOL;
+        echo "住所：".$this->address.PHP_EOL;
+        echo "電話番号：".$this->tel.PHP_EOL;
+    }
+}
+
+class Department extends Company{
+    private $department;
+    private $manager;
+
+    public function __construct($company, $address, $tel, $department, $manager) {
+        parent::__construct($company, $address, $tel);
+        $this->department = $department;
+        $this->manager = $manager;
     }
 
-    if($array[$i] == 13 ){
-        echo $array[$i].PHP_EOL;
-        echo "for_out".PHP_EOL;
-        break;
+    public function displayDepartment(){
+        $this->displayCompany();
+        echo "部署名：".$this->department.PHP_EOL;
+        echo "部長：".$this->manager.PHP_EOL.PHP_EOL;
     }
+}
 
+$sampleA = new Department("サンプルA","市ヶ谷","080-XXXX-XXXX","人事","高田");
+$sampleB = new Department("サンプルB","池袋","090-XXXX-XXXX","総務","田中");
+$sampleC = new Department("サンプルC","新宿","070-XXXX-XXXX","営業","高橋");
+
+
+$instances = array($sampleA,$sampleB,$sampleC);
+
+foreach($instances as $instance){
+    $instance->displayDepartment();
 }
 
 ?>
